5bbc3523a6ae35a928966bedddd93757a63011bf
novianto778 <63406837+Novianto778@users.noreply.github.com>
2025-03-24 15:31:52 +0700

    feat: add commitlint setup

---
+++ commitlint.config.ts
@@ -0,0 +1,11 @@
+import type { UserConfig } from '@commitlint/types';
+
+const config: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']],
+    'subject-case': [2, 'always', 'sentence-case'],
+  },
+};
+
+export default config;
