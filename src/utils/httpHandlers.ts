0d2139e4eb2175c3d87151ddf3b63c8e22c140b3
novianto778 <63406837+Novianto778@users.noreply.github.com>
2025-04-10 15:06:40 +0700

    fix: ts config and httpHandlers any type

--- src/utils/httpHandlers.ts
+++ src/utils/httpHandlers.ts
@@ -1,8 +1,8 @@
 import type { Response } from 'express';
 import { ServiceResponse } from '../model/serviceResponse';
 
-export const handleServiceResponse = (
-  serviceResponse: ServiceResponse<any>,
+export const handleServiceResponse = <T>(
+  serviceResponse: ServiceResponse<T>,
   response: Response,
 ) => {
   return response.status(serviceResponse.statusCode).send(serviceResponse);
